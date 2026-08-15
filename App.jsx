f4beb431b5d2882abe2d444992d3ba7147ccc945
BeOTian <145973402+BeOTian@users.noreply.github.com>
2026-08-15 17:21:33 +0700

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+export default function App() {
+  return (
+    <div className="landing-page">
+      <h1>Paradise Nursery</h1>
+      <button>Get Started</button>
+    </div>
+  );
+}
