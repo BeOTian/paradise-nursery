dad7e7cb0513df02fcd9a475a07b59c4057fd160
BeOTian <145973402+BeOTian@users.noreply.github.com>
2026-08-15 17:21:01 +0700

    Create AboutUs.jsx

---
+++ AboutUs.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+export default function AboutUs() {
+  return (
+    <div>
+      <h1>About Paradise Nursery</h1>
+      <p>Welcome to our company. We provide the best plants.</p>
+    </div>
+  );
+}
